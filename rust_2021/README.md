3907593ddf97b60771079b811943563c873e6c17
Cameron Wright <ccameronwright@gmail.com>
2023-11-30 21:28:22 -0700

    Added work on Day 3 2021.

---
+++ rust_2021/README.md
@@ -0,0 +1,14 @@
+# Advent of Code 2021
+
+## Requirements
+
+- Rust
+
+## Build
+
+
+## Running
+
+```bash
+cargo run -- [-e --each] [-d --day <DAY>] [-i --init]
+```
--- rust_2021/src/solutions/day_03.rs
+++ rust_2021/src/solutions/day_03.rs
@@ -1,12 +1,44 @@
 #![warn(unused_mut)]
 pub fn binary_diagnostic(input: &String) -> i32 {
-    // let mut number = ;
+    println!("'{}'", input);
+    let mut vec = input.split("\n").map(|x| i32::from_str_radix(x, 2).unwrap() ).collect::<Vec<i32>>();
+    let mut gamma_rate = calc_gamma_rate(&vec);
+    let number_bits = 5;  // TODO Need to have the bit length calculated, sample is 5 the actual is 12
+    // // Find bit size of largest number, or just assume for now that its alwasy 5
+    // for n in (0..number_bits).rev() {
+    //     let mut common_bit = 0;
+    //     for i in &vec {
+    //         println!("{} => {:05b}", i, i);
+    //         // print_type_of(&i);
+    //         let bit = i & (1 << n);  // We don't need to shift, just check if the number is not 0
+    //         if bit > 0 {
+    //             common_bit += 1;
+    //         } else {
+    //             common_bit -= 1;
+    //         }
+    //         // println!("Most significant bit is {}", bit);
+    //     }
+    //     // println!("common_bit is {}", common_bit);
+    //     if common_bit > 0 {
+    //         gamma_rate = gamma_rate | (1 << n);
+    //     } else {
+    //         gamma_rate = gamma_rate | (0 << n);
+    //     }
+    //     // println!("gamma_rate is {:05b}", gamma_rate);
+    // }
+    let base: i32 = 2;
+    let epsilon_rate = !gamma_rate & (base.pow(number_bits) - 1);
+    println!("gamma_rate is {}, epsilon_rate is {}", gamma_rate, epsilon_rate);
+    return gamma_rate * epsilon_rate;
+}
+
+fn calc_gamma_rate(input: &Vec<i32>) -> i32 {
     let mut gamma_rate = 0;
     let number_bits = 5;  // TODO Need to have the bit length calculated, sample is 5 the actual is 12
     // Find bit size of largest number, or just assume for now that its alwasy 5
     for n in (0..number_bits).rev() {
         let mut common_bit = 0;
-        for i in input.split("\n").map(|x| isize::from_str_radix(x, 2).unwrap() ) {
+        for i in input {
             // println!("{} => {:05b}", i, i);
             // print_type_of(&i);
             let bit = i & (1 << n);  // We don't need to shift, just check if the number is not 0
@@ -25,39 +57,8 @@ pub fn binary_diagnostic(input: &String) -> i32 {
         }
         // println!("gamma_rate is {:05b}", gamma_rate);
     }
-    let base: i32 = 2;
-    let epsilon_rate = !gamma_rate & (base.pow(number_bits) - 1);
-    println!("gamma_rate is {}, epsilon_rate is {}", gamma_rate, epsilon_rate);
-    return gamma_rate * epsilon_rate;
+    return gamma_rate;
 }
-
-// fn calc_gamma_rate(input: &Vec<i32>) -> i32 {
-//     let mut gamma_rate = 0;
-//     let number_bits = 5;  // TODO Need to have the bit length calculated, sample is 5 the actual is 12
-//     // Find bit size of largest number, or just assume for now that its alwasy 5
-//     for n in (0..number_bits).rev() {
-//         let mut common_bit = 0;
-//         for i in input {
-//             // println!("{} => {:05b}", i, i);
-//             // print_type_of(&i);
-//             let bit = i & (1 << n);  // We don't need to shift, just check if the number is not 0
-//             if bit > 0 {
-//                 common_bit += 1;
-//             } else {
-//                 common_bit -= 1;
-//             }
-//             // println!("Most significant bit is {}", bit);
-//         }
-//         // println!("common_bit is {}", common_bit);
-//         if common_bit > 0 {
-//             gamma_rate = gamma_rate | (1 << n);
-//         } else {
-//             gamma_rate = gamma_rate | (0 << n);
-//         }
-//         // println!("gamma_rate is {:05b}", gamma_rate);
-//     }
-//     return gamma_rate;
-// }
 
 fn calc_oxygen_generator_rating(input: &Vec<i32>) -> i32 {
     if input.len() == 1 {
