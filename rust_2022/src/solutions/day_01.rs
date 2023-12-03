eb1a54bc191283209746224e3bce5bbd22bba811
Cameron Wright <ccameronwright@gmail.com>
2023-12-02 17:36:44 -0700

    2022-day01: Reworked day 1 to use functional sums and max functions instead of doing it myself.

--- rust_2022/src/solutions/day_01.rs
+++ rust_2022/src/solutions/day_01.rs
@@ -1,62 +1,58 @@
 pub fn problem1(input: &String) -> i32 {
-    let mut sum = 0;
-    let mut max_sum = 0;
-    // Read the file line by line using the lines() iterator from std::io::BufRead.
-    for line in input.lines() {
-        if line.chars().count() == 0 {
-            println!("Sum {}", sum);
-            if sum > max_sum {
-                max_sum = sum;
-            }
-            sum = 0;  // Sum should be restarted 
-        } else {
-           sum += line.parse::<i32>().unwrap(); 
-        }
-    }
-    return max_sum
+    let elves = input.split("\n\n").map(| elf |  {
+        elf.split("\n").map(| food | food.parse::<i32>().unwrap() ).sum::<i32>()
+    }).max().unwrap();
+    return elves;
 }
 
-
 pub fn problem2(input: &String) -> i32 {
-    let mut sum = 0;
+    let elves = input.split("\n\n").map(| elf |  {
+        elf.split("\n").map(| food | food.parse::<i32>().unwrap() ).sum::<i32>()
+    });
     // Replace this maxSum with a Array containing 3 spots, starting each with 0
-    let mut max_sums_array: [i32; 3] = [0,0,0];
+    let mut max_sums: [i32; 3] = [0,0,0];
     // let max_sum: i32 = 0;
     
     // Read the file line by line using the lines() iterator from std::io::BufRead.
-    for line in input.lines() {
-        if line.chars().count() == 0 {
-            println!("Finished Elf");
-            add_num_to_array(sum,&mut max_sums_array);
-            sum = 0;  // Sum should be restarted
+    for elf in elves {
+        if elf < max_sums[0]{
+            continue;
+        }else if elf <= max_sums[1] {
+            max_sums[0] = elf;
+       } else if elf <= max_sums[2] {
+        max_sums[0] = max_sums[1];
+        max_sums[1] = elf;
         } else {
-           sum += line.parse::<i32>().unwrap(); 
+            max_sums[0] = max_sums[1];
+            max_sums[1] = max_sums[2];
+           max_sums[2] = elf;
         }
     }
-    return sum_array(&max_sums_array);
+    return max_sums.iter().sum();
 }
 
-fn add_num_to_array(n: i32, arr: &mut [i32]) {
-    println!("Number to add: {}, Array {:?}", n, arr); 
-    //TODO Get this refactored into loop
-    if n < arr[0]{
-        return;
-    }else if n <= arr[1] {
-        arr[0] = n;
-   } else if n <= arr[2] {
-       arr[0] = arr[1];
-       arr[1] = n;
-    } else {
-       arr[0] = arr[1];
-       arr[1] = arr[2];
-       arr[2] = n;
-    }
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
 
-fn sum_array(arr: &[i32]) -> i32 {
-    let mut max_sum = 0;
-    for i in 0..arr.len() {
-        max_sum += arr[i];
+    //Arrange
+    //Act
+    //Assert
+
+    #[test]
+    fn test_problem1() {
+        let input =
+            fs::read_to_string("data/sample/day_01.txt").expect("Data file doesn't exist!");
+        let expected = 24000;
+        assert_eq!(problem1(&input), expected);
     }
-    return max_sum;
+
+    #[test]
+    fn test_problem2() {
+        let input =
+            fs::read_to_string("data/sample/day_01.txt").expect("Data file doesn't exist!");
+        let expected = 45000;
+        assert_eq!(problem2(&input), expected);
+    }
 }
