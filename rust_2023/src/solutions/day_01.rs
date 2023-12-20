214e4a56d2d013c82772eb76a007ff18e96c2c0f
Cameron Wright <ccameronwright@gmail.com>
2023-12-19 19:16:23 -0700

    2023: Added day 01 and day 02 unit tests to include actual results.

--- rust_2023/src/solutions/day_01.rs
+++ rust_2023/src/solutions/day_01.rs
@@ -91,7 +91,29 @@ fn add_and_sum_lines(input: &Vec<String>) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde::Deserialize;
+    use serde_json;
     use std::fs;
+
+    #[derive(Deserialize, Debug)]
+    struct Solution {
+        id: String,
+        first: i64,
+        second: i64,
+    }
+
+    fn get_solution(day: String, problem: i8) -> i64 {
+        let json_string =
+            fs::read_to_string("data/solutions.json").expect("JSON file doesn't exist!");
+        let json: Vec<Solution> =
+            serde_json::from_str(&json_string).expect("JSON was not well-formatted");
+        let solution = json.iter().find(|x| x.id == day).unwrap();
+        return if problem == 1 {
+            solution.first
+        } else {
+            solution.second
+        };
+    }
 
     //Arrange
     //Act
@@ -99,18 +121,28 @@ mod tests {
 
     #[test]
     fn test_problem1() {
+        // Sample
         let input =
             fs::read_to_string("data/sample/day_01_sample.txt").expect("Data file doesn't exist!");
         let expected = 142;
         assert_eq!(problem1(&input), expected);
+        //Actual
+        let input = fs::read_to_string("data/day_01.txt").expect("Data file doesn't exist!");
+        let expected = get_solution("day01".to_string(), 1);
+        assert_eq!(problem1(&input) as i64, expected);
     }
 
     #[test]
     fn test_problem2() {
+        // Sample
         let input =
             fs::read_to_string("data/sample/day_01_sample2.txt").expect("Data file doesn't exist!");
         let expected = 281;
         assert_eq!(problem2(&input), expected);
+        //Actual
+        let input = fs::read_to_string("data/day_01.txt").expect("Data file doesn't exist!");
+        let expected = get_solution("day01".to_string(), 2);
+        assert_eq!(problem2(&input) as i64, expected);
     }
 
     #[test]
